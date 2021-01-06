42ec26fd853f2126cc927e57832ebbe64373ad90
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-01-06 15:11:23 +0000

    Update task-3.2.3.js

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -1,6 +1,14 @@
-function fullName(first, last) {
-  // ADD YOUR CODE BELOW HERE
-  console.log(first + ' ' + last);
-  // ADD YOUR CODE ABOVE HERE
-  
+/* EXAMPLE */
+
+function greet(greeting, name) {
+  return greeting + ' ' + name;
 }
+
+var message = greet('Hello', 'Luke');
+    console.log( message ); // Output: Hello Luke
+
+/* EXAMPLE */
+
+// ADD YOUR CODE BELOW HERE
+
+// ADD YOUR CODE ABOVE HERE
