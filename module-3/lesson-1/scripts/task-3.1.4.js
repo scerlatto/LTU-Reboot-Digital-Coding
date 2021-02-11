be7fe1f197864fe6d832c5f55506ede9e5a738aa
Scarlett <77694370+scerlatto@users.noreply.github.com>
2021-02-11 20:53:06 +0000

    Update task-3.1.4.js
    Added variable and output via console.log and document.write for solving a simple equation.

---
+++ module-3/lesson-1/scripts/task-3.1.4.js
@@ -0,0 +1,11 @@
+var catsInHats = 10;
+
+var dogsInLogs = 4;
+
+var result = catsInHats + dogsInLogs;
+
+console.log(result); // leads to output that displays the sum of cats/dogs
+document.write(result);
+
+
+
