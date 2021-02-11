647e3bdf5e2dc0236bf8852361d21bd150955222
Scarlett <77694370+scerlatto@users.noreply.github.com>
2021-02-11 20:11:12 +0000

    Update task-3.1.2.js
    added more detail to comments.

--- module-3/lesson-1/scripts/task-3.1.2.js
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -9,4 +9,4 @@ this is a multi-line comment in my code
 
 alert('hello world'); // this pops up with an alert message
 console.log('hello world'); // this shows a message in the console
-document.write('hello from the document.write') // prints a message to the screen
+document.write('<p>hello from the document.write</p>') // prints a message to the screen. can use <p></p> tags for styling
