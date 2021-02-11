a5d1475bec9af41ae8cc607c1a53761acb594399
Scarlett <77694370+scerlatto@users.noreply.github.com>
2021-02-11 21:10:30 +0000

    Update task-3.1.5.js
    Completed variable concation of firstname and surname.

---
+++ module-3/lesson-1/scripts/task-3.1.5.js
@@ -0,0 +1,9 @@
+var firstName = 'Douglas ';
+var fullName = firstName + 'Douglason';
+
+console.log(fullName) //outputs Douglas Douglason
+
+var firstName = 'Marjorie ';
+firstName += 'Marjerine';
+
+console.log(firstName) //outputs Marjorie Marjerine 
