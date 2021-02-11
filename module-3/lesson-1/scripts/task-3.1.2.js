01bd4479122d0b44d3fa30faa18446859cab4abd
Scarlett <77694370+scerlatto@users.noreply.github.com>
2021-02-11 20:10:00 +0000

    Update task-3.1.2.js
    Added comments to code. Added document.write functions along with alert and console.log

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,12 @@
+/*
+
+this is a multi-line comment in my code 
+this is a multi-line comment in my code
+
+*/
+
+// this is a single line comment in my code. 
+
+alert('hello world'); // this pops up with an alert message
+console.log('hello world'); // this shows a message in the console
+document.write('hello from the document.write') // prints a message to the screen
