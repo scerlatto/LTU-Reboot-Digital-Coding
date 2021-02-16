b29bc9b2bac64658a95b0bd4fbd6cb63ebb78929
Scarlett <77694370+scerlatto@users.noreply.github.com>
2021-02-16 19:21:04 +0000

    Update task-3.2.1.js
    Added functions to javascript

---
+++ module-3/lesson-2/scripts/task-3.2.1.js
@@ -0,0 +1,9 @@
+function sentence() {   //declaring a function
+
+    console.log('this is a sentence');
+}
+
+sentence(); //invoking a function - call a function 
+sentence(); //invoking a function - call a function 
+sentence(); //invoking a function - call a function 
+sentence(); //invoking a function - call a function 
