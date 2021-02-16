315adb99ebe2fe655ffecde6c2f5fd3c4e34c242
Scarlett <77694370+scerlatto@users.noreply.github.com>
2021-02-16 19:38:26 +0000

    Update task-3.2.2.js
    Updated the function to use multiple arguments.

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,8 @@
+function fullName( firstName, lastName ) {
+
+    console.log( firstName + ' ' + lastName ); //''concatonates an extra space
+}
+
+fullName ('Luke' , 'Ashton-Johnson');
+fullName ('Peter' , 'Jones');
+fullName ('Neil', 'Armstrong'); 
