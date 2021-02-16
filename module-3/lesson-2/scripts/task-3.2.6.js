fa4a6cdd35152db755fc77558d5d9091fd936871
Scarlett <77694370+scerlatto@users.noreply.github.com>
2021-02-16 20:44:35 +0000

    Update task-3.2.6.js
    Included two variables in the program with a logical operator

---
+++ module-3/lesson-2/scripts/task-3.2.6.js
@@ -0,0 +1,21 @@
+var temperature = 51;
+var season = 'summer';
+
+if ( temperature < 0 ) {
+    console.log('stay inside'); 
+
+} else if (temperature < 30 ) { 
+
+    console.log('wear a coat and a hat');
+}
+
+else if (temperature > 50 && season === 'summer' ) { 
+   
+    console.log('dont forget your sunglasses');
+}
+
+else {
+    console.log( 'just pants and a vest is fine');
+}
+
+//two variables are included with the inclusion of a logical operator. 
