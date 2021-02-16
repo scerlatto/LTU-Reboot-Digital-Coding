e0770dcb8765e1110f562402256581b9a3e9ba9a
Scarlett <77694370+scerlatto@users.noreply.github.com>
2021-02-16 20:09:30 +0000

    Update task-3.2.3.js
    added a return statement to name function, used that function to set the value of a variable

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -11,4 +11,19 @@ var message = greet('Hello', 'Luke');
 
 // ADD YOUR CODE BELOW HERE
 
+function fullName( firstName, lastName ) {
+
+  return firstName + ' ' + lastName ; //''concatonates an extra space
+}
+
+var person1 = fullName('Luke', 'Ashton');
+
+console.log( fullName('Luke' , 'Ashton') );
+console.log( fullName('Peter', 'James') );
+console.log( fullName('Grace', 'Perry') ); 
+
+console.log( 'person1 variable: ' + person1);
+
 // ADD YOUR CODE ABOVE HERE
+
+
