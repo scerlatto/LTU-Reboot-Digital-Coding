f50eff5bc4759d08ab8f651965c7bf2c3feae498
Scarlett <77694370+scerlatto@users.noreply.github.com>
2021-02-18 19:58:44 +0000

    Update task-3.3.1.js
    Created a function to calculate the percentage of a number

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,12 @@
+function percentageCalculator ( number, percentage ) {
+
+    var percentageOf = ( number * percentage ) / 100;
+
+return percentageOf;
+
+}
+
+console.log ( percentageCalculator( 135, 30 ) ); 
+
+
+
