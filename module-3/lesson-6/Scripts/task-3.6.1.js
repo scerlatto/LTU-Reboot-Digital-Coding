27e32774ac11fd1df3a0da775eb327387f4b11d7
Scarlett <77694370+scerlatto@users.noreply.github.com>
2021-03-02 19:53:26 +0000

    task 3.6.1
    Added styling of ID/Classes/elements via javascript

---
+++ module-3/lesson-6/Scripts/task-3.6.1.js
@@ -0,0 +1,19 @@
+let paragraphs = document.querySelectorAll("p"); 
+
+let paragraph3 = paragraphs[2];
+    paragraph3.style.border = "4px solid red";
+    paragraph3.style.padding = "20px";
+
+let logo = document.querySelector["img"];
+    logo.setAttribute( "src", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.i3T9XbkrRSnvM3kqiF04OgHaHa%26pid%3DApi&f=1");
+
+let courseList = document.querySelectorAll("li");
+
+let courseListItem2 = courseList[1]; //1 is the index number 
+    courseListItem2.style.color = "green";
+    courseListItem2.style.fontsize = "36px";
+
+
+
+
+
